362ecd68bbcc31003ac27cc288304d28ab443a58
LOuLOu-THEKing <78346797+LOuLOu-THEKing@users.noreply.github.com>
2024-03-30 21:28:33 +0530

    Add files via upload

---
+++ content.py
@@ -0,0 +1,26 @@
+Help = '''```Hi!!! Welcome to Mind Breaker!!
+We are just trying to make a cheap dank memer!
+Here is a list of commands to use:
+	2. !tst - starts type speed test
+	3. !gmlt - gives the list of all games
+	4. !lb - provides your points with your rank!   
+	5. !commandhelp - helps you give the info we are giving right now!```'''
+
+GameList = '''```
+Giving you a list of games and their commands!
+1. !tictactoe <@opponent> - starts playing tic tac toe!
+2. !ttt - starts playing tic tac toe with the computer!
+3. !hangman - starts playing hangman!
+```
+'''
+
+Dm_Content = '''
+```
+We hope you have fun time here!!
+Here are the rules and regulations:
+1. Be kind
+2. No Racism is allowed, Results automatically in a ban
+3. abide by the rules from the rules and regulations page or it will result in a warning. (3 warnings and you are banned.)
+```
+'''
+
